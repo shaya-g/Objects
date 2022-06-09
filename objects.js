55dd1c8932fb92a916fe271cd6d026674f9bb8a5
Shaya Golafshani <s.golaff@gmail.com>
2022-06-09 10:58:30 -0600

    Update the objects section

--- objects.js
+++ objects.js
@@ -5,32 +5,36 @@
 // the desired result. To check your work, run this
 // file in your Terminal.
 
-foods = {
-  apples: 23, 
-  grapes: 507, 
-  eggs: 48
-}
+var foods = {
+  apples: 23,
+  grapes: 507,
+  eggs: 48,
+  oranges: 56
+};
 
-// Write code that prints all of the 'keys' of the foods variable 
+// Write code that prints all of the 'keys' of the foods variable
 // you created above:
 // YOUR CODE HERE
+console.log(Object.keys(foods));
 
-// Write code that prints all of the 'values' of the foods variable 
+// Write code that prints all of the 'values' of the foods variable
 // you created above:
 // YOUR CODE HERE
+console.log(Object.values(foods));
 
-// Write code that prints the value of the second food of the foods variable 
+// Write code that prints the value of the second food of the foods variable
 // you created above:
 // YOUR CODE HERE
+console.log(foods.grapes);
 
-// Write code that adds a food to the foods object. 
+// Write code that adds a food to the foods object.
 // Then, print the updated object:
 // YOUR CODE HERE
+console.log("oranges: 56");
 
 
-#-------------------
 // Part 2: Email
-#-------------------
+
 
 
 // Think about all the pieces of information associated with one single email in your inbox.
@@ -39,21 +43,32 @@ foods = {
 // Declare a variable that stores an object. Each key should be an attribute of an email and each
 // value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
+var email = {
+  sender: "Billy Bob Joe",
+  receiver: "Shaya G",
+  subject: "How are you today?",
+  timeSent: "10:27 AM",
+  attachments: false
+};
+
 // Write code that logs your email object to the terminal.
+console.log(email);
 
-
-// Write code that logs all of the 'keys' of the email object 
+// Write code that logs all of the 'keys' of the email object
 // you created above:
 // YOUR CODE HERE
+console.log(Object.keys(email));
 
-// Write code that logs all of the 'values' of the email object 
+// Write code that logs all of the 'values' of the email object
 // you created above:
 // YOUR CODE HERE
+console.log(Object.values(email));
 
 
-#-------------------
+
+
 // Part 3: Many Emails - CHALLENGE!
-#-------------------
+
 
 // LONG EXAMPLE:
 // Now that we've learned about Objects AND Arrays, we can combine them.
@@ -69,14 +84,14 @@ posts = ["image at beach", "holiday party", "adorable puppy", "video of cute bab
 // that relate to each individual Instagram post:
 
 
-posts = [
+var posts = [
   {
     imageSrc: "./images/beach.png",
     caption: "At the beach with my besties",
     timestamp: "4:37 PM August 13, 2019",
     number_likes: 0,
     comments: []
-  }, 
+  },
   {
     imageSrc: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -90,12 +105,38 @@ console.log(posts);
 console.log(posts[0]);
 
 
-// The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-// Objects. Each of those Objects has 4 key-value pairs. This may LOOK 
+// The code snippet above shows an Array with 2 elements. Each element in the Array is a
+// Objects. Each of those Objects has 4 key-value pairs. This may LOOK
 // a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 // good to have some exposure before going into Mod 1.
 
 
-// YOU DO: Create an array of at least 3 EMAIL Objects, using the same 
+// YOU DO: Create an array of at least 3 EMAIL Objects, using the same
 // key-value pairs you used in your email Object above.
 // Then, print the email Array to the Terminal.
+
+var emailArray = [
+  {
+    sender: "David Gordon",
+    receiver: "Hazel Pablo",
+    subject: "Important Meeting",
+    timeSent: "3:00 PM",
+    attachments: true
+  },
+  {
+    sender: "Jacob Zimmer",
+    receiver: "Angie Staffieri",
+    subject: "Question on Mod 0",
+    timeSent: "11:27 AM",
+    attachments: false
+  },
+  {
+    sender: "Amazon",
+    receiver: "Shaya Golafshani",
+    subject: "Would You Like to Work With Us?",
+    timeSent: "9:00 AM",
+    attachments: false
+  }
+];
+
+console.log(emailArray)
